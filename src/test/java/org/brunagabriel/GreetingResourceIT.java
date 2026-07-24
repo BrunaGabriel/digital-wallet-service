ef2391a0d6e18cf889a4fe2fdeeb3f438c25f1bc
Bruna Gabriel <b._gabriel@hotmail.com>
2026-07-24 10:48:06 +0100

    first commit

---
+++ src/test/java/org/brunagabriel/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.brunagabriel;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
